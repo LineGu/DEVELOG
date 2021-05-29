f4d42020ec1b653fc49547c49373d50112f40ba0
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-05-29 21:14:59 +0900

    Feat : :zap:️ blog Page 생성
    page 생성 및 Theme button 생성

--- client/src/components/blog/Page/Blog.tsx
+++ client/src/components/blog/Page/Blog.tsx
@@ -1,12 +1,19 @@
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 import PageBase from '@components/common/PageBase/PageBase';
 import Header from '@components/common/Header';
+import ThemeButton from '@components/common/ThemeBtn';
 import { IPagePorps } from '@types';
+
+const StyledThemeBtn = styled(ThemeButton)`
+  margin: 76vh 0 0 5vw;
+`;
 
 function BlogPageComponent({ mode }: IPagePorps): ReactElement {
   return (
     <PageBase mode={mode} title="Develog">
       <Header currentPage="blog" />
+      <StyledThemeBtn />
     </PageBase>
   );
 }
